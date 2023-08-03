c8ed5ffc85e8676478593ac08ad833c60d6cc6d2
Eric Baer <me@ericbaer.com>
2023-08-03 15:23:14 -0700

    Add default value + README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hello World
+
+This model prints the input string. That's it.
+
+## Usage
+
+**Use the default input**
+
+```sh
+cog predict
+# Hello, World!
+```
+
+**Pass an input parameter**
+
+```sh
+cog predict -i text="Go go gadget, Replicate!"
+# Go go gadget, Replicate!
+```
--- predict.py
+++ predict.py
@@ -2,5 +2,7 @@ from cog import BasePredictor, Input
 
 
 class Predictor(BasePredictor):
-    def predict(self, text: str = Input(description="Text to print")) -> str:
+    def predict(
+        self, text: str = Input(description="Text to print", default="Hello, World!")
+    ) -> str:
         return text
